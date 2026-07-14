35e049b048a6d6bfa304c56271c21ff96c4fd368
Thandokuhle18 <maphangathandokuhle@gmail.com>
2026-07-14 03:32:58 +0200

    Create InfoCard component

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,17 @@
+
+
+function InfoCard({ user }) {
+    return <div className="info-card">
+        <div className="name-card">
+            <h2>Name: {user.name} </h2>
+        </div>
+        <div className="email-card">
+            <h3>Email: {user.email} </h3>
+        </div>
+        <div className="companyName-card">
+            <h3>Company Name: {user.company.name} </h3>
+        </div>
+    </div>
+}
+
+export default InfoCard
